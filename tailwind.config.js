4f28f4f4a026694f2bee6a26a6f4a1c7e67c453a
matheusOliv23 <matheushenri1996@gmail.com>
2023-02-18 14:45:41 -0300

    feat: add theme and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
         "./src/components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "gray-50": "#fafafa",
+                "gray-100": "#f1f1f1",
+                "dark-50:": "#101010",
+                "orange-50": '#fbaf85',
+                "orange-100": "#D87D4A"
+            }
+
+        },
+        fontFamily: {
+            manrope: ['Manrope', 'sans-serif']
+        }
     },
     plugins: [],
 }
