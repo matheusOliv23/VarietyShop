f0e6e1e0a3933a923774a7066f24ffb512accdd8
matheusOliv23 <matheushenri1996@gmail.com>
2023-02-27 18:38:42 -0300

    test: testing product item

---
+++ src/components/ProductItem/test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react"
+
+import ProductItem from "./index"
+
+const { getByRole, getByText } = screen
+
+export const produtItem = {
+  id: "1",
+  name: "XX99 MARK II",
+  image: "/images/ear_small.svg",
+  price: 250
+}
+
+describe("<ProductItem />", () => {
+  it("should render by default", () => {
+    render(
+      <ProductItem
+        id={produtItem.id}
+        name={produtItem.name}
+        price={produtItem.price}
+        image={produtItem.image}
+      />
+    )
+
+    const name = getByText(produtItem.name)
+    const price = getByText(String(produtItem.price))
+    const img = document.querySelector("img") as HTMLImageElement
+    const button = getByRole("button", { name: "SEE PRODUCT" })
+
+    expect(name).toBeInTheDocument()
+    expect(price).toBeInTheDocument()
+    expect(button).toBeInTheDocument()
+    expect(img.src).toContain(produtItem.image)
+    expect(img.alt).toContain(produtItem.name)
+  })
+})
