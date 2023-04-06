5ef20c050fefaad5630d0545f7e165635ee46130
matheusOliv23 <matheushenri1996@gmail.com>
2023-04-06 18:36:52 -0300

    feat: create product page

--- src/components/InfoSection/index.tsx
+++ src/components/InfoSection/index.tsx
@@ -7,7 +7,7 @@ interface InfoSectionProps {
 }
 export default function InfoSection({ text, title, image }: InfoSectionProps) {
   return (
-    <section className="flex items-center justify-center md:justify-between px-6 gap-4 md:gap-10 md:flex-row flex-col my-10 max-w-6xl m-auto">
+    <section className="flex items-center justify-center md:justify-between px-6 gap-4 md:gap-10 md:flex-row flex-col my-10 max-w-7xl m-auto">
       <div className="md:w-[445px] text-center md:text-left">
         <h2 className="font-bold mb-8 uppercase text-4xl">{title}</h2>
         <p className="font-medium opacity-40">{text}</p>
---
+++ src/pages/product/index.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link"
+import React from "react"
+import Layout from "src/components/Layout"
+import Image from "next/image"
+import Button from "src/components/Button"
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai"
+
+export default function Product() {
+  return (
+    <Layout>
+      <div className="max-w-7xl m-auto">
+        <Link href={"/"} className="mt-20" passHref>
+          Go back
+        </Link>
+
+        <section className="mt-14 flex md:flex-row flex-col items-center justify-between">
+          <div className="md:w-[370px] lg:w-[540px] flex justify-center items-center bg-gray-100 md:h-[380px] lg:h-[560px] w-4/5">
+            <Image
+              width={350}
+              height={350}
+              src={"/images/phone.svg"}
+              alt={"product.name"}
+            />
+          </div>
+
+          <div className={`text-center lg:text-left`}>
+            <p
+              className={`${"text-orange-100"} text-sm opacity-50 tracking-[10px]`}
+            >
+              NEW PRODUCT
+            </p>
+            <h1
+              className={`${"text-black"} font-bold text-4xl md:text-6xl my-6 max-w-[396px]`}
+            >
+              XX99 Mark II Headphones
+            </h1>
+            <p
+              className={`${"text-black"} font-medium opacity-75 max-w-sm m-auto lg:m-0 !mb-10`}
+            >
+              The new XX99 Mark II headphones is the pinnacle of pristine audio.
+              It redefines your premium headphone experience by reproducing the
+              balanced depth and precision of studio-quality sound.
+            </p>
+
+            <p className="font-bold text-xl mb-12">$ 2,999</p>
+
+            <div className="flex items-center lg:justify-start justify-center gap-4">
+              <div className="flex w-[120px] h-12 justify-around items-center bg-gray-100">
+                <AiOutlinePlus />
+                <span>1</span>
+                <AiOutlineMinus />
+              </div>
+              <Button variant="PRIMARY">ADD TO CART </Button>
+            </div>
+          </div>
+        </section>
+      </div>
+    </Layout>
+  )
+}
